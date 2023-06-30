f6446f66ad6efae5eb279644cb0fafdbd525cb37
MADERU-CM <cliv.moyia@student.moringaschool.com>
2023-06-30 09:25:13 +0300

    Done Assignment

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const cleanedWord = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+  return cleanedWord === cleanedWord.split('').reverse().join('');
 }
-
-/* 
-  Add your pseudocode here
-*/
+const word1 = "abba";
+const word2 = "robot";
+const word3 = "racecar";
+const word4 = "ab";
+const word5 = "a";
+console.log(isPalindrome(word1)); 
+console.log(isPalindrome(word2));
+console.log(isPalindrome(word3)); 
+console.log(isPalindrome(word4));
+console.log(isPalindrome(word5)); 
 
 /*
-  Add written explanation of your solution here
+  The isPalindrome function takes a word as input, converts it to lowercase, removes non-alphanumeric characters, and then checks if the cleaned word is the same when read forwards and backwards. If it is, the function returns true, indicating that the word is a palindrome. Otherwise, it returns false.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  //console.log("Expecting: true");
+  //console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  //console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  //console.log("Expecting: false");
+  //console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
